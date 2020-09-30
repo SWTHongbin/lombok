1a8b43a3937363f4b9a6d51176405af57fdc7845
马再平 <mazaiping@51zhaoyou.com>
2020-09-30 17:40:06 +0800

    With code and desc

---
+++ src/core/lombok/WithCodeAndDesc.java
@@ -0,0 +1,14 @@
+package lombok;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface WithCodeAndDesc {
+	String codeName() default "code";
+	
+	String descName() default "desc";
+}
